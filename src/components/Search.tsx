88032c38fc1207fd1ebc446538c453daab985d07
Dezolutions <mars92851@gmail.com>
2023-07-27 13:23:00 +0300

    search changes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,27 +7,22 @@ const Search :React.FC = () => {
 
   const {setSearchValue} = useDateStore()
   const [search, setSearch] = React.useState<string>('')
-  console.log(search)
 
-  const handleSearch = (e: any) => {
-    console.log(e.target.value)
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
     setSearchValue(e.target.value)
   }
 
   return (
-
-    <>
-      <DebounceInput
-        css={searchInputStyle}
-        minLength={1}
-        debounceTimeout={300}
-        placeholder="Enter task title..."
-        type="text"
-        value={search}
-        onChange={handleSearch}
-      />
-    </>
+    <DebounceInput
+      css={searchInputStyle}
+      minLength={1}
+      debounceTimeout={300}
+      placeholder="Enter task title..."
+      type="text"
+      value={search}
+      onChange={handleSearch}
+    />
   )
 }
 
